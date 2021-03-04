4e5fb4510862fb5a85c4779ab7a542fbabee7293
achan <tapitapeh@gmail.com>
2021-03-04 23:18:58 +0800

    update

--- components/Author.vue
+++ components/Author.vue
@@ -1,11 +1,12 @@
 <template>
   <div
+    v-if="item"
     class="container mx-auto px-8 flex flex-col items-center justify-center text-center space-y-2"
   >
     <p class="uppercase tracking-widest text-xs">Penulis</p>
-    <img :src="item.image.url" alt="" class="w-80 h-80" />
-    <p class="text-lg font-bold">{{ item.name }}</p>
-    <p class="text-sm">{{ item.description }}</p>
+    <img v-if="item.image" :src="item.image.url" alt="" class="w-80 h-80" />
+    <p v-if="item.name" class="text-lg font-bold">{{ item.name }}</p>
+    <p v-if="item.description" class="text-sm">{{ item.description }}</p>
   </div>
 </template>
 
